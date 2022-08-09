3dd58a1e496f1b719fcf5bb5053aa63ba8a98c0f
Vladyslav Kolesnyk <v.kolesn1k369@gmail.com>
2022-08-09 23:32:22 +0300

    json parse implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -10,7 +12,10 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,6 +23,8 @@ public class Main {
         List<Person> personList = csvParse.transform(readFile(fileName), Person.class);
         String pathFile = "src/main/resources/sample.xml";
         List<Person> personList1 = xmlParse.transform(readXml(pathFile), Person.class);
+        String pathFile2 = "src/main/resources/sample.json";
+        List<Person> personList2 = jsonParse.transform(jsonToMap(pathFile2), Person.class);
     }
 
     public static List<String> readFile(String file) {
@@ -40,5 +47,20 @@ public class Main {
         } catch (ParserConfigurationException | SAXException | IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    private static Map<String, JsonNode> jsonToMap(String fileName) {
+        JsonNode jsonTreeNode;
+        try {
+            jsonTreeNode = new ObjectMapper().readTree(new File(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Iterator<String> fields = jsonTreeNode.fieldNames();
+        Map<String, JsonNode> map = new LinkedHashMap<>();
+        while (fields.hasNext()) {
+            String name = fields.next();
+            map.put(name, jsonTreeNode.get(name));
+        }
+        return map;
     }
 }
